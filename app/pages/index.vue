a958d1c03d1d95df2d2b83a4255ffd06ed8484ff
sheh <shezw@foxmail.com>
2025-08-03 11:37:42 +0800

    fix: color mode

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
 const { x, y } = useMouse()
-const color = useColorMode()
-
-function toggleDark() {
-  color.value = color.value === 'dark'
-    ? 'light'
-    : 'dark'
-}
 </script>
 
 <template>
   <div grid="~ cols-[1fr_2fr]" flex-grow>
     <div text-red border="r base" p-4>
       Hello World {{ x }}, {{ y }}<br>
-
-      <button @click="toggleDark">
-        {{ color.value }}
-      </button>
     </div>
     <ThePlayground />
   </div>
