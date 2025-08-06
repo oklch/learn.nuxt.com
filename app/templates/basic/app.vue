1901795cae310dd9f53fa434a1de7dcd95ffe1a6
sheh <shezw@foxmail.com>
2025-08-06 16:30:20 +0800

    fix(template): move communication logic to plugin

--- app/templates/basic/app.vue
+++ app/templates/basic/app.vue
@@ -1,22 +1,3 @@
-<script setup lang="ts">
-// Communicate with parent window for navigation
-if (import.meta.client) {
-  onMounted(() => {
-    const route = useRoute()
-    watch(
-      () => route.fullPath,
-      (newFullPath) => {
-        window.parent.postMessage({
-          type: 'update:path',
-          path: newFullPath,
-        }, '*')
-      },
-      { immediate: true },
-    )
-  })
-}
-</script>
-
 <template>
   <NuxtPage />
 </template>
