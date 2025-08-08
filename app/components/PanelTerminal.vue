679c25382eb34acf59659fe6aaab40cd9523ed83
sheh <shezw@foxmail.com>
2025-08-08 16:38:43 +0800

    feat: toggle terminal

--- app/components/PanelTerminal.vue
+++ app/components/PanelTerminal.vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+const ui = useUiState()
+</script>
+
 <template>
   <div h-full grid="~ rows-[min-content_1fr]">
-    <div flex="~ gap-2 items-center" border="b base dashed" px4 py2 bg-faded>
+    <div flex="~ gap-2 items-center" border="b base dashed" p2 pl4 bg-faded>
       <div i-ph-terminal-window-duotone />
       <span text-sm>Terminal</span>
+      <div flex-auto />
+      <button hover="bg-active" p1 rounded @click="ui.toggleTerminal()">
+        <div i-ph-x-bold />
+      </button>
     </div>
     <PanelTerminalClient />
   </div>
--- app/components/TheNav.vue
+++ app/components/TheNav.vue
@@ -1,9 +1,21 @@
+<script setup lang="ts">
+const ui = useUiState()
+</script>
+
 <template>
   <nav flex="~ gap-1 items-center" p="x4 y3" border="b base">
     <NuxtLink to="/" title="Nuxt Playground">
       <NuxtPlaygroundLogo class="h-2em" />
     </NuxtLink>
     <div flex-auto />
+    <button
+      p2 rounded
+      hover="bg-active"
+      :class="ui.showTerminal ? '' : 'op50'"
+      @click="ui.toggleTerminal()"
+    >
+      <div i-ph-terminal-window-duotone text-2xl />
+    </button>
     <ColorSchemeToggle />
     <NuxtLink
       p-2 rounded hover:bg-active
